6d9e64db9736fcb8322350452a498779a37a6cd3
sukhman603 <74083904+sukhman603@users.noreply.github.com>
2020-11-09 19:25:59 -0800

    Create numbers.php

---
+++ numbers.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title> numbers/title>
+</head>
+<body>
+<?php  
+$number=1233456;  
+if($number%2==0)  
+{  
+ echo "$number is Even Number";   
+}  
+else  
+{  
+ echo "$number is Odd Number";  
+}   
+?>  
+</body>
+</html>
